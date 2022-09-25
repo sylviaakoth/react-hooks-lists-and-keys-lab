c45cd40e6fc265623b77a9dce1dc6e7584784209
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-09-25 20:50:51 +0300

    Completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
-    </div>
-  );
+	return (
+		<div className="project-item">
+			<h3>{name}</h3>
+			<p>{about}</p>
+			<div className="technologies">
+				{technologies.map((tech) => (
+					<span key={tech}>{tech}</span>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ProjectItem;
